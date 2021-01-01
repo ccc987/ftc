cc4c467796e91fa6dc92de201f0d530e14c18232
ccc987 <ccc987@gmail.com>
2021-01-01 14:12:59 -0500

    Update StrafingTestWorking.java

--- StrafingTestWorking.java
+++ StrafingTestWorking.java
@@ -195,8 +195,6 @@ public class StrafingTestWorking extends OpMode {
     }
 
 
-
-    }
     double getBatteryVoltage() {
         double result = Double.POSITIVE_INFINITY;
         for (VoltageSensor sensor : hardwareMap.voltageSensor) {
