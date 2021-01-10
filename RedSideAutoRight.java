62ad3136c6437dedfdfbda502008762d3a60cfd6
ccc987 <ccc987@gmail.com>
2021-01-09 19:13:59 -0500

    update for red

---
+++ RedSideAutoRight.java
@@ -0,0 +1,447 @@
+/* Copyright (c) 2019 FIRST. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted (subject to the limitations in the disclaimer below) provided that
+ * the following conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright notice, this list
+ * of conditions and the following disclaimer.
+ *
+ * Redistributions in binary form must reproduce the above copyright notice, this
+ * list of conditions and the following disclaimer in the documentation and/or
+ * other materials provided with the distribution.
+ *
+ * Neither the name of FIRST nor the names of its contributors may be used to endorse or
+ * promote products derived from this software without specific prior written permission.
+ *
+ * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
+ * LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.VoltageSensor;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
+
+import org.firstinspires.ftc.robotcore.external.ClassFactory;
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer;
+import org.firstinspires.ftc.robotcore.external.tfod.Recognition;
+import org.firstinspires.ftc.robotcore.external.tfod.TFObjectDetector;
+
+import java.util.List;
+
+/**
+ * This 2020-2021 OpMode illustrates the basics of using the TensorFlow Object Detection API to
+ * determine the position of the Ultimate Goal game elements.
+ * <p>
+ * Use Android Studio to Copy this Class, and Paste it into your team's code folder with a new name.
+ * Remove or comment out the @Disabled line to add this opmode to the Driver Station OpMode list.
+ * <p>
+ * IMPORTANT: In order to use this OpMode, you need to obtain your own Vuforia license key as
+ * is explained below.
+ */
+@Autonomous(name = "RedSideAutoRight", group = "Concept")
+//@Disabled
+public class RedSideAutoRight extends LinearOpMode {
+    private static final String TFOD_MODEL_ASSET = "UltimateGoal.tflite";
+    private static final String LABEL_FIRST_ELEMENT = "Quad";
+    private static final String LABEL_SECOND_ELEMENT = "Single";
+
+
+    private DcMotor leftWheelF = null;               //Left Wheel Front
+    private DcMotor leftWheelR = null;               //Left Wheel Back
+    private DcMotor rightWheelF = null;              //Right Wheel Front
+    private DcMotor rightWheelR = null;
+    private DcMotor armWheel = null;
+    private DcMotor intakeWheel1 = null;
+    private DcMotor intakeWheel2 = null;
+    //private DcMotor intakeWheel3 = null;
+    private Servo wobbleServoHand = null;
+    private Servo ringPush = null;
+    private DcMotor outtakeWheel1 = null;
+
+
+    /*
+     * IMPORTANT: You need to obtain your own license key to use Vuforia. The string below with which
+     * 'parameters.vuforiaLicenseKey' is initialized is for illustration only, and will not function.
+     * A Vuforia 'Development' license key, can be obtained free of charge from the Vuforia developer
+     * web site at https://developer.vuforia.com/license-manager.
+     *
+     * Vuforia license keys are always 380 characters long, and look as if they contain mostly
+     * random data. As an example, here is a example of a fragment of a valid key:
+     *      ... yIgIzTqZ4mWjk9wd3cZO9T1axEqzuhxoGlfOOI2dRzKS4T0hQ8kT ...
+     * Once you've obtained a license key, copy the string from the Vuforia web site
+     * and paste it in to your code on the next line, between the double quotes.
+     */
+    private static final String VUFORIA_KEY =
+            "AXiCpJb/////AAABmUeqLpvfjkywirbDoSbnyFYKMf7uB24PIfaJZtIqcZO3L7rZVbsKVlz/fovHxEI6VgkUt3PBpXnp+YmHyLrWimMt2AKMFMYsYeZNRmz0p8jFT8DfQC7mmUgswQuPIm64qc8rxwV7vSb0et6Za96tPoDHYNHzhdiaxbI0UHpe4jCkqNTiRDFz8EVNds9kO7bCIXzxBfYfgTDdtjC5JRJ/drtM6DZnTXOqz3pdM85JEVgQqL9wBxUePSjbzyMo9e/FgxluCuWtxHraRJeeuvAlFwAb8wVAoV1cm02qIew0Vh0pDVJqy04gu62CJPhv/wwnXCKywUIEzVMbOLe7muycyHoT6ltpAn4O4s4Z82liWs9x";
+
+
+    /**
+     * {@link #vuforia} is the variable we will use to store our instance of the Vuforia
+     * localization engine.
+     */
+    private VuforiaLocalizer vuforia;
+
+    /**
+     * {@link #tfod} is the variable we will use to store our instance of the TensorFlow Object
+     * Detection engine.
+     */
+    private TFObjectDetector tfod;
+
+    String test = "";
+
+    @Override
+    public void runOpMode() {
+        // The TFObjectDetector uses the camera frames from the VuforiaLocalizer, so we create that
+        // first.
+        initVuforia();
+        initTfod();
+
+
+        leftWheelF = hardwareMap.get(DcMotor.class, "D1");
+        rightWheelF = hardwareMap.get(DcMotor.class, "D2");
+        leftWheelR = hardwareMap.get(DcMotor.class, "D3");
+        rightWheelR = hardwareMap.get(DcMotor.class, "D4");
+        armWheel = hardwareMap.get(DcMotor.class, "A1");
+        intakeWheel1 = hardwareMap.get(DcMotor.class, "I1");
+        intakeWheel2 = hardwareMap.get(DcMotor.class, "I2");
+        wobbleServoHand = hardwareMap.get(Servo.class, "S2");
+        ringPush = hardwareMap.get(Servo.class, "P1");
+        outtakeWheel1 = hardwareMap.get(DcMotor.class, "O1");
+        /**
+         * Activate TensorFlow Object Detection before we wait for the start command.
+         * Do it here so that the Camera Stream window will have the TensorFlow annotations visible.
+         **/
+        if (tfod != null) {
+            tfod.activate();
+
+            // The TensorFlow software will scale the input images from the camera to a lower resolution.
+            // This can result in lower detection accuracy at longer distances (> 55cm or 22").
+            // If your target is at distance greater than 50 cm (20") you can adjust the magnification value
+            // to artificially zoom in to the center of image.  For best results, the "aspectRatio" argument
+            // should be set to the value of the images used to create the TensorFlow Object Detection model
+            // (typically 1.78 or 16/9).
+
+            // Uncomment the following line if you want to adjust the magnification and/or the aspect ratio of the input images.
+            tfod.setZoom(3, 2);
+        }
+
+        /** Wait for the game to begin */
+        telemetry.addData(">", "Press Play to start op mode");
+        double a = getBatteryVoltage();
+        telemetry.addData("voltage", a);
+
+        telemetry.update();
+
+        waitForStart();
+
+        if (opModeIsActive()) {
+            sleep(2000);
+            int r1 = detectRing();
+            telemetry.addData(String.format("  r1 (%d)", 99999), "%d ",
+                    r1);
+            if (r1 == 1) {
+                caseB();
+            } else if (r1 == 4) {
+                caseC();
+            } else {
+                caseA();
+            }
+
+
+        }
+
+
+        if (tfod != null) {
+            tfod.shutdown();
+        }
+    }
+
+
+    private void move(double drive,
+                      double strafe,
+                      double rotate) {
+
+        double powerLeftF;
+        double powerRightF;
+        double powerLeftR;
+        double powerRightR;
+
+        powerLeftF = drive + strafe + rotate;
+        powerLeftR = drive - strafe + rotate;
+
+        powerRightF = drive - strafe - rotate;
+        powerRightR = drive + strafe - rotate;
+
+        leftWheelF.setPower(-powerLeftF);
+        leftWheelR.setPower(-powerLeftR);
+
+        rightWheelF.setPower(powerRightF);
+        rightWheelR.setPower(powerRightR);
+    }
+
+    private void shoot(double intake) {
+        //drive = -gamepad1.left_stick_y;  // Negative because the gamepad is weird
+        //strafe = gamepad1.left_stick_x;
+        //rotate = gamepad1.right_stick_x;
+        double voltageFactor = getFactorOfVoltage();
+        double powerIntake;
+        powerIntake = -intake;
+        intakeWheel1.setPower(powerIntake*voltageFactor);
+        intakeWheel2.setPower(-powerIntake*voltageFactor);
+    }
+
+    private void raiseArm(double raise) {
+        double powerRaise;
+        powerRaise = raise;
+        armWheel.setPower(powerRaise);
+    }
+
+    private void lowerArm(double lower) {
+        double powerLower;
+        powerLower = lower;
+        armWheel.setPower(-powerLower);
+    }
+
+    private void ringPush() {
+        ringPush.setPosition(0.7);
+        sleep(300);
+        ringPush.setPosition(1);
+    }
+
+    private int detectRing() {
+        while (opModeIsActive()) {
+            if (tfod != null) {
+                // getUpdatedRecognitions() will return null if no new information is available since
+                // the last time that call was made.
+                List<Recognition> updatedRecognitions = tfod.getUpdatedRecognitions();
+
+                if (updatedRecognitions != null) {
+                    telemetry.addData("# Object Detected", updatedRecognitions.size());
+                    // step through the list of recognitions and display boundary info.
+                    int i = 0;
+                    for (Recognition recognition : updatedRecognitions) {
+                        telemetry.addData(String.format("label (%d)", i), recognition.getLabel());
+                        test = recognition.getLabel();
+                        telemetry.addLine(test);
+                        telemetry.addData(String.format("test (%d)", i), test);
+                        telemetry.addData(String.format("  left,top (%d)", i), "%.03f , %.03f",
+                                recognition.getLeft(), recognition.getTop());
+                        telemetry.addData(String.format("  right,bottom (%d)", i), "%.03f , %.03f",
+                                recognition.getRight(), recognition.getBottom());
+                    }
+                    telemetry.update();
+
+                    if (test.equals("Single")) {
+                        telemetry.addLine("single ring");
+                        telemetry.update();
+                        return 1;
+                    } else if (test.equals("Quad")) {
+                        telemetry.addLine("quad ring");
+                        telemetry.update();
+                        return 4;
+                    } else {
+                        telemetry.addLine("nothing");
+                        telemetry.update();
+                        return 0;
+                    }
+
+                }
+            }
+        }
+
+        return 0;
+    }
+
+    private void caseB() {
+        wobbleServoHand.setPosition(1);
+        sleep(500);
+        raiseArm(0.5);
+        sleep(200);
+        move(0.75, 0, 0);
+        sleep(3000);
+        move(0, 0, 0);
+        move(0, 0, -0.50);
+        sleep(650);
+        move(0, 0, 0);
+        shoot(1);
+        sleep(1000);
+        ringPush();
+        sleep(1000);
+        shoot(0);
+        move(0, 0, 0.50);
+        sleep(650);
+        move(0.75, 0, 0);
+        sleep(2000);
+        move(0, -0.6, 0);
+        sleep(1750);
+        move(0, 0, 0);
+        sleep(500);
+        lowerArm(0.5);
+        sleep(300);
+        lowerArm(0);
+        wobbleServoHand.setPosition(0);
+        sleep(1000);
+        move(0, 0.5, 0);
+        sleep(1000);
+        move(-0.50, 0, 0);
+        sleep(1000);
+        raiseArm(0.5);
+        sleep(300);
+    }
+
+    private void caseA() {
+        wobbleServoHand.setPosition(1);
+        sleep(500);
+        raiseArm(0.5);
+        sleep(200);
+        move(0.75, 0, 0);
+        sleep(3000);
+        move(0, 0, 0);
+        move(0, 0, -0.50);
+        sleep(650);
+        move(0, 0, 0);
+        shoot(1);
+        sleep(1000);
+        ringPush();
+        sleep(1000);
+        shoot(0);
+        move(0, 0, 0.50);
+        sleep(650);
+        move(0.75, 0, 0);
+        sleep(500);
+        move(0, 0, 0);
+        sleep(500);
+        sleep(300);
+        lowerArm(0.5);
+        sleep(300);
+        lowerArm(0);
+        wobbleServoHand.setPosition(0);
+        sleep(1000);
+        sleep(300);
+    }
+
+    private void caseC() {
+        wobbleServoHand.setPosition(1);
+        sleep(500);
+        raiseArm(0.5);
+        sleep(200);
+        move(0.75, 0, 0);
+        sleep(2000);
+        move(0, 0, 0);
+        move(0, 0, -0.50);
+        sleep(650);
+        move(0, 0, 0);
+        shoot(1);
+        sleep(1000);
+        ringPush();
+        sleep(1000);
+        shoot(0);
+        move(0, 0, 0.50);
+        sleep(650);
+        move(0.75, 0, 0);
+        sleep(4000);
+        move(0, 0, 0);
+        sleep(500);
+        lowerArm(0.5);
+        sleep(300);
+        lowerArm(0);
+        wobbleServoHand.setPosition(0);
+        sleep(1000);
+        move(-0.75, 0, 0);
+        sleep(1750);
+        move(0, 0, 0);
+        raiseArm(0.5);
+        sleep(300);
+    }
+
+    /**
+     * Initialize the Vuforia localization engine.
+     */
+    private void initVuforia() {
+        /*
+         * Configure Vuforia by creating a Parameter object, and passing it to the Vuforia engine.
+         */
+        VuforiaLocalizer.Parameters parameters = new VuforiaLocalizer.Parameters();
+        parameters.vuforiaLicenseKey = VUFORIA_KEY;
+        parameters.cameraName = hardwareMap.get(WebcamName.class, "Webcam 1");
+
+
+        //  Instantiate the Vuforia engine
+        vuforia = ClassFactory.getInstance().createVuforia(parameters);
+
+        // Loading trackables is not necessary for the TensorFlow Object Detection engine.
+    }
+    double getBatteryVoltage() {
+        double result = Double.POSITIVE_INFINITY;
+        for (VoltageSensor sensor : hardwareMap.voltageSensor) {
+            double voltage = sensor.getVoltage();
+            if (voltage > 0) {
+                result = Math.min(result, voltage);
+            }
+        }
+        return result;
+    }
+
+    private double getFactorOfVoltage() {
+        double currentVoltage = getBatteryVoltage();
+        //telemetry.addData("currentVoltage ", "%2.2f", currentVoltage);
+        double mult;
+        double thresholdVoltage = 12;
+        if (currentVoltage >= thresholdVoltage+1.5) {
+            mult = 0.84;
+        } else if (currentVoltage >= thresholdVoltage+1.4) {
+            mult = 0.86;
+        } else if (currentVoltage >= thresholdVoltage+1.3) {
+            mult = 0.88;
+        } else if (currentVoltage >= thresholdVoltage+1.2) {
+            mult = 0.90;
+        } else if (currentVoltage >= thresholdVoltage+1.1) {
+            mult = 0.93;
+        } else if (currentVoltage >= thresholdVoltage+1) {
+            mult = 0.95;
+            //telemetry.addData("multiplier 13.3", "%2.2f", mult);
+            //telemetry.update();
+        } else if (currentVoltage <= thresholdVoltage) {
+            //telemetry.addLine("Change the battery!");
+            mult = 1;
+        } else {
+            mult = 1;
+            //telemetry.addData("multiplier default", "%2.2f", mult);
+            //telemetry.update();
+        }
+
+        //telemetry.addData("multiplier1: ", mult);
+        //telemetry.addData("multiplier end", "%2.2f", mult);
+        //telemetry.update();
+        return mult;
+    }
+
+    /**
+     * Initialize the TensorFlow Object Detection engine.
+     */
+    private void initTfod() {
+        int tfodMonitorViewId = hardwareMap.appContext.getResources().getIdentifier(
+                "tfodMonitorViewId", "id", hardwareMap.appContext.getPackageName());
+        TFObjectDetector.Parameters tfodParameters = new TFObjectDetector.Parameters(tfodMonitorViewId);
+        tfodParameters.minResultConfidence = 0.8f;
+        tfod = ClassFactory.getInstance().createTFObjectDetector(tfodParameters, vuforia);
+        tfod.loadModelFromAsset(TFOD_MODEL_ASSET, LABEL_FIRST_ELEMENT, LABEL_SECOND_ELEMENT);
+    }
+}
